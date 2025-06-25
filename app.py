4e6fcd15c580e8fda2c91d3b52069bb7e4eaa465
bsharma-code <bs8209661553@gmail.com>
2025-06-25 23:46:11 +0530

    curd operation in flask

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    disc = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"{self.sno} - {self.title}"
+
+@app.route("/", methods=['GET', 'POST'])
+def post_funtion():
+    if request.method == 'POST':
+        title = request.form['title']
+        disc = request.form['disc']
+        todo = Todo(title=title, disc=disc)
+        db.session.add(todo)
+        db.session.commit()
+    allTodo = Todo.query.all()
+    return render_template('index.html', allTodo=allTodo)
+
+@app.route("/delete/<int:sno>")
+def delete_funtion(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
+
+@app.route("/update/<int:sno>", methods=['GET', 'POST'])
+def update_function(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    if request.method == 'POST':
+        todo.title = request.form['title']
+        todo.disc = request.form['disc']
+        db.session.commit()
+        return redirect("/")
+    return render_template('update.html', todo=todo)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
